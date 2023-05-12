7352b649dac88eca8efa77a792ddfe59baa7a9b4
Shota Pailodze <55694002+shotapailodze@users.noreply.github.com>
2023-05-12 16:22:42 +0400

    progress bar done

--- script.js
+++ script.js
@@ -47,6 +47,13 @@ function updateProgress() {
     duration.textContent = `${displayTime(video.duration)}`;
 }
 
+// Click to seek
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+}
+
 // Volume Controls --------------------------- //
 
 
@@ -63,3 +70,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
